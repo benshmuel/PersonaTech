37ad8ca6894771e1ac5ab0e472aef43b1d1d202d
benshmuel <bshmuel19@gmail.com>
2018-01-14 13:09:45 +0200

    Fixing welcome page of the server

--- src/main/java/ServerPackage/MainClass.java
+++ src/main/java/ServerPackage/MainClass.java
@@ -22,7 +22,7 @@ public class MainClass {
 
         //working on DB is below //
 
-        BasicConfigurator.configure();
+        //BasicConfigurator.configure();
         for(int i=0;i<20;i++)
             System.out.println("\n");
 
