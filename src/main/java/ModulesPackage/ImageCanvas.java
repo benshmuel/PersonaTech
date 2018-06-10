ae190f725cef005a3e846644010664943c2f1b3a
benshmuel <bshmuel19@gmail.com>
2018-06-10 15:52:28 +0300

    adding python to server

--- src/main/java/ModulesPackage/ImageCanvas.java
+++ src/main/java/ModulesPackage/ImageCanvas.java
@@ -25,7 +25,7 @@ public class ImageCanvas implements Serializable {
     }
 
     public void writeObject(ObjectOutputStream out) throws IOException {
-        out.defaultWriteObject();
+        //out.defaultWriteObject();
         out.writeInt(images.size()); // how many images are serialized?
         for (BufferedImage eachImage : images) {
             ImageIO.write(eachImage, "png", out); // png
